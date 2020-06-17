dbec415d23f453ffec662567e9b43070c738b64b
esterjo <edsterjo@gmail.com>
2020-06-17 18:24:55 -0400

    finishedfirstarticle

---
+++ blogdown/post/001_Deriving_PCA/2015-07-23-r-rmarkdown_cache/html/unnamed-chunk-1_sourceCpp/sourceCpp-x86_64-w64-mingw32-1.0.4.6/sourcecpp_3cac749959f8/file3cac3dc763b4.cpp.R
@@ -0,0 +1,5 @@
+`.sourceCpp_1_DLLInfo` <- dyn.load('C:/Users/edste/Documents/Blog/content/post/001_Deriving_PCA/2015-07-23-r-rmarkdown_cache/html/unnamed-chunk-1_sourceCpp/sourceCpp-x86_64-w64-mingw32-1.0.4.6/sourcecpp_3cac749959f8/sourceCpp_2.dll')
+
+EigenDecomp <- Rcpp:::sourceCppFunction(function(M) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_EigenDecomp')
+
+rm(`.sourceCpp_1_DLLInfo`)

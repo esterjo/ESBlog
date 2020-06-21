09a398532a910e306c3aceaaba705a88f3457279
esterjo <edsterjo@gmail.com>
2020-06-21 19:19:46 -0400

    updates

---
+++ blogdown/post/002_HMC_RcppEigen/main_cache/html/my_chunk_sourceCpp/sourceCpp-x86_64-w64-mingw32-1.0.4.6/sourcecpp_32fc52dc38b8/file32fc2bcf7169.cpp.R
@@ -0,0 +1,5 @@
+`.sourceCpp_1_DLLInfo` <- dyn.load('C:/Users/edste/Documents/Blog/content/post/002_HMC_RcppEigen/main_cache/html/my_chunk_sourceCpp/sourceCpp-x86_64-w64-mingw32-1.0.4.6/sourcecpp_32fc52dc38b8/sourceCpp_2.dll')
+
+timesTwo <- Rcpp:::sourceCppFunction(function(x) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_timesTwo')
+
+rm(`.sourceCpp_1_DLLInfo`)
